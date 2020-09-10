9dc206a2e9f87035318e1198ac2060e924cf8886
David <davidmcruz1125@gmail.com>
2020-09-10 14:32:30 -0500

    Implement sql database creation

--- server/schema.sql
+++ server/schema.sql
@@ -1,12 +1,39 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- CREATE TABLE rooms (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   name CHAR (255),
+--   PRIMARY KEY( id )
+-- );
+
+-- CREATE TABLE users (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   name CHAR (255),
+--   PRIMARY KEY( id )
+-- );
+
+-- CREATE TABLE messages (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   text CHAR (255),
+--   user_id INT,
+--   room_id INT,
+--   PRIMARY KEY( id ),
+--   FOREIGN KEY (user_id)
+--     REFERENCES users(id),
+--   FOREIGN KEY (room_id)
+--     REFERENCES rooms(id)
+-- );
+
+CREATE TABLE friends (
+  id INT NOT NULL,
+  name CHAR (255)
 );
 
 /* Create other tables and define schemas for them here! */
+
+
 
 
 
